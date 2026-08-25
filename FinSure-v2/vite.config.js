011b849e7700c09f44f04c1f01b0a1fd92a46f2d
Balaji4723 <balajimaninadar4712@gmail.com>
2026-08-25 23:48:11 +0530

    Code splitting - no build warnings

--- FinSure-v2/vite.config.js
+++ FinSure-v2/vite.config.js
@@ -4,4 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            { name: 'react-vendor',  test: /node_modules\/(react|react-dom|react-router)/ },
+            { name: 'three-vendor',  test: /node_modules\/(three|@react-three)/ },
+            { name: 'firebase',      test: /node_modules\/(firebase)/ },
+            { name: 'motion',        test: /node_modules\/(framer-motion)/ },
+            { name: 'pdf',           test: /node_modules\/(jspdf|html2canvas)/ },
+          ]
+        }
+      }
+    }
+  }
 })
